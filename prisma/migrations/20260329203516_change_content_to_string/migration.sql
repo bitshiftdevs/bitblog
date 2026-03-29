c369cd0454d5524e7e1b6fd2b20ad447edb4fada
Kratosgado <mbeahessilfieprince@gmail.com>
2026-03-29 20:39:25 +0000

    feat: use markdown string for post content instead of json

---
+++ prisma/migrations/20260329203516_change_content_to_string/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "public"."post_revisions" ALTER COLUMN "content" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "public"."posts" ALTER COLUMN "content" SET DATA TYPE TEXT;
